4818522244671863eb3942487e952db7a0902135
psilotec <scottadevito@gmail.com>
2016-11-07 19:12:33 -0500

    Validate beltsData object before insertion in belts.Insert

--- imports/collections/belts.js
+++ imports/collections/belts.js
@@ -1,9 +1,12 @@
 // Collection of belts and beltIds
 
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check'
 
 Meteor.methods({
   'belts.insert': function (beltsData) {
+
+    check(beltsData, Object);
 
     // Ensure that only server can invoke this method
     if(this.connection == null) {
