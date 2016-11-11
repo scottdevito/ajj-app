766b4dbd8a4caa9b91ab544a4023fc209d08b690
psilotec <scottadevito@gmail.com>
2016-11-11 13:29:01 -0500

    Import belts info from json and insert into Belts

--- server/main.js
+++ server/main.js
@@ -5,5 +5,15 @@ Meteor.startup(() => {
   // Publish list of belts/belt info
   Meteor.publish('belts', function() {
     return Belts.find({});
+
+
+    // Insert inital belt data into the db if it's not already inserted
+    if (Belts.find().count() === 0) {
+      const beltsData = JSON.parse(Assets.getText('belts.json'));
+
+      beltsData.map((item) => {
+        Meteor.call('belts.insert', item);
+      });
+    }
   });
 });
