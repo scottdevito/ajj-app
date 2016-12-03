f62830937ffe195c27db95809193f186d3aa0d9d
psilotec <scottadevito@gmail.com>
2016-12-03 12:41:07 -0500

    Wire up basic left and right technique navigation

--- client/components/techniques/technique.js
+++ client/components/techniques/technique.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react';
 import TechniqueCard from './technique_card';
+import { browserHistory } from 'react-router';
 
 class Technique extends Component {
-  // Event handler for left and right buttons
-  handleNavigationClick(direction, event) {
-    console.log(`${direction} navigation button clicked`);
+  // Event handler for left and right navigation buttons
+  handleNavigationClick(direction, techId, event) {
+    let prevTechId = parseInt(techId) - 1,
+        nextTechId = parseInt(techId) + 1,
+        browseableNextTechId = techId;
 
-    // Check if the next technique exists in the color belt (if it doesn't, check if it exists at all then prompt the user if they want to go to the next belt)
+    // Function to pad browseableNextTechId so it can be used by the router
+    function padTechId (techId) {
+      if (techId <= 9999) { techId = ("000" + techId).slice(-4); }
+      return techId.toString();
+    }
+
+    // Add or subtract one from current techId based on which direction was clicked
+    if (direction === "left" && techId !== "0000") {
+      browseableNextTechId = padTechId(prevTechId);
+    } else if (direction === "right") {
+      browseableNextTechId = padTechId(nextTechId);
+    }
+
+    // Navigate to the next technique
+    browserHistory.push(`/technique/${browseableNextTechId}`);
 
     // Validate the user to see if they have access to this technique
 
     // Move the preview slider to the right or left depending on which direction was passed
 
-    // Load new technique info depending on which direction was passed
-
   }
 
   render() {
+    let techId = this.props.params.techId;
     return (
       <div className="technique-flex">
         <div className="arrows">
-          <i className="huge chevron left icon link" onClick={this.handleNavigationClick.bind(this, "left")}></i>
+          <i className="huge chevron left icon link" onClick={this.handleNavigationClick.bind(this, "left", techId)}></i>
         </div>
 
         <div className="technique-card">
-          <TechniqueCard techId={this.props.params.techId} />
+          <TechniqueCard techId={techId} />
         </div>
 
         <div className="arrows">
-          <i className="huge chevron right icon link" onClick={this.handleNavigationClick.bind(this, "right")}></i>
+          <i className="huge chevron right icon link" onClick={this.handleNavigationClick.bind(this, "right", techId)}></i>
         </div>
       </div>
     );
