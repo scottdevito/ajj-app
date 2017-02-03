40bd6cd6d4f75f7254b7064923582ed720ed0656
psilotec <scottadevito@gmail.com>
2017-02-02 23:28:02 -0500

    Pull/display data from DB based on modalTechId

--- client/components/techniques/edit_technique_modal.js
+++ client/components/techniques/edit_technique_modal.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Techniques } from '../../../imports/collections/techniques';
 
 class EditTechniqueModal extends Component {
     render() {
+        // Variable to keep track of when currentTechniqueInfo query is finished so it's data can be rendered
+        let modalCurrentTechniqueInfoIsNotNull = this.props.modalCurrentTechniqueInfo != null;
+        
         return(
             <div className="ui modal">
                 <i className="close icon"></i>
@@ -10,9 +15,14 @@ class EditTechniqueModal extends Component {
                 </div>
                 <div className="image content">
                     <div className="description">
-                    <div className="ui header">We've auto-chosen a profile image for you.</div>
-                    <p>We've grabbed the following image from the image associated with your registered e-mail address.</p>
-                    <p>Is it okay to use this photo?</p>
+                        <div className="ui header">Title:</div>
+                            <p>{ modalCurrentTechniqueInfoIsNotNull ?
+                                <input type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techName} /> : '' }
+                            </p>
+                        <div className="ui header">Description:</div>
+                            <p>{ modalCurrentTechniqueInfoIsNotNull ?
+                                <textarea type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techDesc} /> : '' }
+                            </p>   
                     </div>
                 </div>
                 <div className="actions">
@@ -29,4 +39,9 @@ class EditTechniqueModal extends Component {
     }
 }
 
-export default EditTechniqueModal;
+// Subscription to get technique info based on which modalTechId is passed
+export default createContainer((props) => {
+  Meteor.subscribe('techniques');
+
+  return { modalCurrentTechniqueInfo: Techniques.findOne({ 'techId': props.modalTechId }) };
+}, EditTechniqueModal);
