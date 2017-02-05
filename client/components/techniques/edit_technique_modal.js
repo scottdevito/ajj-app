22e859709a647a34242cff06ddb7be8d6f34c2e2
psilotec <scottadevito@gmail.com>
2017-02-04 19:55:21 -0500

    Add image preview and change image URL field

--- client/components/techniques/edit_technique_modal.js
+++ client/components/techniques/edit_technique_modal.js
@@ -21,18 +21,24 @@ class EditTechniqueModal extends Component {
                 <div className="image content">
                     <div className="description">
                         <div className="ui header">Title:</div>
-                            <p>{ modalCurrentTechniqueInfoIsNotNull ?
-                                <input type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techName} /> : '' }
-                            </p>
+                        <p>{modalCurrentTechniqueInfoIsNotNull ?
+                            <input type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techName} /> : '' }
+                        </p>
+                        <div className="ui header">Image URL:</div>
+                        <p>{modalCurrentTechniqueInfoIsNotNull ?
+                            <input type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techImgURL} /> : '' }
+                        </p>                            
                         <div className="ui header">Description:</div>
-                            <p>{ modalCurrentTechniqueInfoIsNotNull ?
-                                <textarea type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techDesc} /> : '' }
-                            </p>   
+                        <p>{modalCurrentTechniqueInfoIsNotNull ?
+                            <textarea type='text' size='50' value={this.props.modalCurrentTechniqueInfo.techDesc} /> : '' }
+                        </p>   
+                    </div>
+                    <div className="ui small image">
+                        <img src="/images/avatar/large/chris.jpg" />
                     </div>
                 </div>
                 <div className="actions">
-                    <div className="ui black deny button" onClick={this.handleCloseModal.bind(this)}
-                    >
+                    <div className="ui black deny button" onClick={this.handleCloseModal.bind(this)}>
                         Cancel
                     </div>
                     <div className="ui positive right labeled icon button">
